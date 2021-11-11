b301ec1abfba124d0e72832df05e7780cc912c4e
AbuHuraira00125 <92776480+AbuHuraira00125@users.noreply.github.com>
2021-11-10 22:19:51 -0800

    gpa
    gpa

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,70 @@
+import 'package:bmi_calculator/cgpa.dart';
+import 'package:bmi_calculator/gpa_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:bmi_calculator/screens/input_page.dart';
 
-void main() => runApp(BMICalculator());
+void main() {
+  runApp(MaterialApp(
+    title: "CGPA Calculator",
+    home: homePage(),
+  ));
+}
 
-class BMICalculator extends StatelessWidget {
+class homePage extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Select what you want"),
+        backgroundColor: Colors.deepPurple,
       ),
-      home: InputPage(),
+
+      drawer: Drawer(
+
+
+
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+              ),
+              padding: EdgeInsets.only(top: 60, left: 20),
+              child: Text('AH CGPA App ' ,  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25 , color: Colors.white),),
+            ),
+            ListTile(
+              title: const Text('GPA' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => GpaMenu()));
+              },
+            ),
+            ListTile(
+              title: const Text('SGPA',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SGPA()));
+              },
+            ),
+            ListTile(
+              title: const Text('CGPA',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SGPA()));
+              },
+            ),
+          ],
+        ),
+
+      ),
+
+
+
+
     );
   }
 }
